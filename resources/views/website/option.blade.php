22a41fee8027a669d1b92de8e46d2cf001548620
marcosborg <m.borges.mail@gmail.com>
2024-09-13 13:10:16 +0100

    Update

--- resources/views/website/option.blade.php
+++ resources/views/website/option.blade.php
@@ -17,25 +17,9 @@
                 </div>
             </div>
             <div class="mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus metus lacus, ultrices nec dapibus
-                vitae, tincidunt a nisl. In arcu diam, laoreet sollicitudin ligula in, posuere finibus mi. Maecenas
-                lacus velit, volutpat non feugiat non, sodales eu tellus. Sed finibus mi sed ligula congue dapibus.
-                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum
-                ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus posuere lorem nec
-                fringilla malesuada. Sed ac nunc tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-                tincidunt ac felis feugiat efficitur. Vestibulum tristique iaculis tellus at dapibus. Nam dapibus urna
-                tellus, et porta sapien mollis vitae. Ut convallis ante eros, mattis cursus orci congue a. Suspendisse
-                sit amet euismod quam.
+                {!! $option->longtext !!}
             </div>
-        </div>
-    </div>
-    <div class="row mt-4">
-        <div class="col">
-            {!! $option->longtext !!}
         </div>
     </div>
 </div>
 @endsection
-<script>
-    console.log({!! $option !!})
-</script>
